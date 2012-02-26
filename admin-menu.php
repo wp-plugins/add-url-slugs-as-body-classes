63d08b614f829f0b71f25533a31a85047a358ba0
aarontgrogg <aarontgrogg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-26 20:36:42 +0000

    Minor correction
    git-svn-id: https://plugins.svn.wordpress.org/add-url-slugs-as-body-classes/tags/1.0@510802 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin-menu.php
+++ admin-menu.php
@@ -14,19 +14,22 @@
 //	Add deconstructed URI as <body> classes
 	function bc_add_body_class( $classes)  { // $classes = array of additional classes to add
 		global $post;
-		foreach((get_the_category($post->ID)) as $category) {
-			$classes[] = $category->category_nicename;
+		if ($post && $post->ID) {
+			foreach((get_the_category($post->ID)) as $category) {
+				$classes[] = trim($category->category_nicename);
+			}
+			$url = get_bloginfo('url');
+			$protocol = (strpos(strtolower($_SERVER['SERVER_PROTOCOL']),'https') === FALSE) ? 'http' : 'https';
+			$base = $protocol .'://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+			$bodyclass = trim(str_replace($url,'',$base));
+			$bodyclass = explode('/',$bodyclass);
+			foreach($bodyclass as $category) {
+				$classes[] = trim($category);
+			}
 		}
-		$url = get_bloginfo('url');
-		$protocol = (strpos(strtolower($_SERVER['SERVER_PROTOCOL']),'https') === FALSE) ? 'http' : 'https';
-		$base = $protocol .'://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-		$bodyclass = trim(str_replace($url,'',$base));
-		$bodyclass = explode('/',$bodyclass);
-		foreach($bodyclass as $category) {
-			$classes[] = $category;
-		}
-		return $classes;
+		return array_unique($classes);
 	}
-    add_filter( 'body_class', 'bc_add_body_class' );
+	add_filter('post_class', 'bc_add_body_class');
+	add_filter('body_class', 'bc_add_body_class');
 
 ?>
