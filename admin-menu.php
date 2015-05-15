1f92e872aa8cdd2a7bac14e5a7a78fbc3726e33c
aarontgrogg <aarontgrogg@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-15 20:26:27 +0000

    Tested on 4.2.2 and added a few FAQs
    git-svn-id: https://plugins.svn.wordpress.org/add-url-slugs-as-body-classes/tags/1.3@1161460 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin-menu.php
+++ admin-menu.php
@@ -4,32 +4,51 @@
 	Plugin Name: Add URL Slugs as Body Classes
 	Plugin URI: http://aarontgrogg.com/2012/02/24/wordpress-plugin-add-url-slugs-as-body-classes/
 	Description: Add page name and/or category slugs as additional `&lt;body&gt;` classes.
-	Version: 1.1
+	Version: 1.3
 	Author: Aaron T. Grogg
 	Author URI: http://aarontgrogg.com/
 	License: GPLv2 or later
 */
 
+	// this only applies for non-admin pages
+	if ( !is_admin() && !function_exists( 'USBC_add_body_class' ) ) :
 
-//	Add deconstructed URI as <body> classes
-	function bc_add_body_class( $classes)  { // $classes = array of additional classes to add
-		global $post;
-		if ($post && $post->ID) {
-			foreach((get_the_category($post->ID)) as $category) {
-				$classes[] = trim($category->category_nicename);
-			}
-			$url = get_bloginfo('url');
-			$protocol = (strpos(strtolower($_SERVER['SERVER_PROTOCOL']),'https') === FALSE) ? 'http' : 'https';
-			$base = $protocol .'://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-			$bodyclass = trim(str_replace($url,'',$base));
-			$bodyclass = explode('/',$bodyclass);
-			foreach($bodyclass as $category) {
-				$classes[] = trim($category);
-			}
+
+		//	Add deconstructed URI as <body> classes
+		//	$classes = array of classes WP is already planning to add
+		function USBC_add_body_class( $classes )  {
+
+			// get the global post variable
+			global $post;
+
+			// make sure we're on a post page
+			if ($post && $post->ID) :
+
+				// loop through any categories
+				foreach( get_the_category($post->ID) as $category ) {
+					// and push the trimmed version to the $classes array
+					$classes[] = trim( $category->category_nicename );
+				}
+
+				// get the current page's URI (the part _after_ your domain name)
+				$uri = $_SERVER["REQUEST_URI"];
+				// explode that string into an array of "pieces"
+				$bodyclass = explode('/',$uri);
+				// loop through those pieces and push each into the $classes array
+				foreach($bodyclass as $category) {
+					$classes[] = trim($category);
+				}
+
+			endif; // $post...
+
+			// return a unique-onlye version of the array to WP
+			return array_unique($classes);
+		
 		}
-		return array_unique($classes);
-	}
-	add_filter('post_class', 'bc_add_body_class');
-	add_filter('body_class', 'bc_add_body_class');
+
+		add_filter('post_class', 'USBC_add_body_class');
+		add_filter('body_class', 'USBC_add_body_class');
+
+	endif; // !is_admin...
 
 ?>
